0d1e1c3069dfdf954fb7d19f120a97ba0e6c4d28
Naina Verma <itsnainav008@gmail.com>
2025-05-14 03:08:12 +0530

    base class updated

--- src/test/java/base/BaseTests.java
+++ src/test/java/base/BaseTests.java
@@ -1,7 +1,9 @@
 package base;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.Dimension;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class BaseTests
@@ -13,7 +15,11 @@ public class BaseTests
         driver = new ChromeDriver();
         driver.get("https://the-internet.herokuapp.com/");
 
-        driver.manage().window().setSize(new Dimension(375,812));
+//      driver.manage().window().setSize(new Dimension(375,812));
+        driver.manage().window().maximize();
+
+        WebElement inputsLink = driver.findElement(By.linkText("Inputs"));
+        inputsLink.click();
 
         System.out.println(driver.getTitle());
         driver.quit();
