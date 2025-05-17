c12f098be9be2491a33544b818006283d40cee0d
Naina Verma <itsnainav008@gmail.com>
2025-05-18 00:11:46 +0530

    removed code to call HomePage before every test, as it is handled in BaseClass

--- src/test/java/keys/KeysTests.java
+++ src/test/java/keys/KeysTests.java
@@ -9,21 +9,17 @@ import pages.KeyPressesPage;
 import static org.testng.Assert.assertEquals;
 
 public class KeysTests extends BaseTests {
-    KeyPressesPage keyPage;
-
-    @BeforeClass
-    public void keyPage(){
-        keyPage = homePage.clickKeyPresses();
-    }
 
     @Test
     public void testBackspace(){
+        KeyPressesPage keyPage = homePage.clickKeyPresses();
         keyPage.enterText("A" + Keys.BACK_SPACE);
         assertEquals(keyPage.getResult(),"You entered: BACK_SPACE", "Key press result is wrong");
     }
 
     @Test
     public void testSpecialCharacters(){
+        KeyPressesPage keyPage = homePage.clickKeyPresses();
         keyPage.enterText(Keys.chord(Keys.SHIFT,"8","5","4"));
     }
 }
