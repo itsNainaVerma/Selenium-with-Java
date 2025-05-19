5de3e909bda8d48a53dfea69fd7957ec1e9420ac
Naina Verma <itsnainav008@gmail.com>
2025-05-20 01:20:45 +0530

    added method to take sccreeshots for tests

--- src/test/java/base/BaseTests.java
+++ src/test/java/base/BaseTests.java
@@ -1,16 +1,24 @@
 package base;
 
+import com.google.common.io.Files;
 import org.openqa.selenium.Dimension;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.ITestResult;
 import org.testng.annotations.*;
 import pages.HomePage;
+import utils.WindowManager;
+
+import java.io.File;
+import java.io.IOException;
 
 public class BaseTests {
     private WebDriver driver;
     protected HomePage homePage;
 
-    @BeforeTest
+    @BeforeClass
     public void setUp() {
         System.setProperty("webdriver.chrome.driver", "resources/chromedriver.exe");
         driver = new ChromeDriver();
@@ -25,8 +33,35 @@ public class BaseTests {
         driver.get("https://the-internet.herokuapp.com/");
     }
 
-    @AfterTest
+    @AfterClass
     public void tearDown() {
         driver.quit();
     }
+
+    @AfterMethod
+    public void takeScreenshotOfFailedTests(ITestResult result) {
+        if (ITestResult.FAILURE == result.getStatus()) {
+            TakesScreenshot camera = (TakesScreenshot) driver;
+            File screenshot = camera.getScreenshotAs(OutputType.FILE);
+            try {
+                Files.move(screenshot, new File("resources/screenshots/" + result.getName() + ".png"));
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            System.out.println("Screenshot taken: " + screenshot.getAbsolutePath());
+        }
+
+    }
+
+//    public void takeScreenshotForAllTests(){
+//        TakesScreenshot camera = (TakesScreenshot) driver;
+//        File screenshot = camera.getScreenshotAs(OutputType.FILE);
+//        try {
+//            Files.move(screenshot, new File("resources/screenshots/test.png"));
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        }
+//        System.out.println("Screenshot taken: " + screenshot.getAbsolutePath());
+//    }
+
 }
